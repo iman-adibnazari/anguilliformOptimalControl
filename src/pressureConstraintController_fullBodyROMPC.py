fix bugs in centerline computation and state estimation; add notebook to verify state estimation block in control loop

src/pressureConstraintController_fullBodyROMPC.py
@@ -5,8 +5,10 @@ import Sofa.constants.Key as Key
 import numpy as np
 from dotenv import dotenv_values 
 from rhcPolicy_ERA import rhcPolicy_ERA
+from rhcPolicy_randomizedOutput import rhcPolicy_randomizedOutput
 from stateEstimator_ERA import stateEstimator_ERA
 import logging
+import h5py
 
 config = dotenv_values(".env")
 ''' 
@@ -24,7 +26,7 @@ class PressureConstraintController_fullBodyROMPC(Sofa.Core.Controller):
         self.chambers = chambers
         self.saveOutput=saveOutput
         self.segments = segments
-        self.rhcPolicy = rhcPolicy_ERA(deltaT,T=self.T, systemMatFile = config["currentDirectory"]+"data/archivedDataSets/FullAssembly_Constrained_FullSetForICRA/romSystemMatricesAndGains_22dim_3train_2test.mat")
+        self.rhcPolicy =  rhcPolicy_randomizedOutput(deltaT,T=self.T, systemMatFile = config["currentDirectory"]+"data/archivedDataSets/FullAssembly_Constrained_FullSetForICRA/romSystemMatricesAndGains_22dim_3train_2test.mat") #rhcPolicy_ERA(deltaT,T=self.T, systemMatFile = config["currentDirectory"]+"data/archivedDataSets/FullAssembly_Constrained_FullSetForICRA/romSystemMatricesAndGains_22dim_3train_2test.mat")
         self.stateEstimator = stateEstimator_ERA(deltaT, logResults = True, systemMatFile = config["currentDirectory"]+"data/archivedDataSets/FullAssembly_Constrained_FullSetForICRA/romSystemMatricesAndGains_22dim_3train_2test.mat")
         self.pressureConstraints = [chamber.getObject('SurfacePressureConstraint') for chamber in self.chambers]
         for pressureConstraint in self.pressureConstraints:
@@ -38,7 +40,7 @@ class PressureConstraintController_fullBodyROMPC(Sofa.Core.Controller):
         self.avgMatrix = np.zeros((self.n_redCenterline,self.n_redLocal))
         self.redCenterlineOffset = np.zeros((self.n_redCenterline*2)) 
         print(self.name.getValueString().__str__())
-
+        self.saveVerificationSet = True
 
     # helper function to define reference trajectories
     # Function to provide coordinates of discretized centerline at a given time
@@ -83,7 +85,6 @@ class PressureConstraintController_fullBodyROMPC(Sofa.Core.Controller):
             y_ref[:,i] = self.generateReferenceCoords(time = time+i,numPoints=numPoints,a_max=a_max,l=l,k=k,omega=omega,x_shift=x_shift,z_shift=z_shift).reshape(-1,1).squeeze()
         return y_ref
 
-
     def onAnimateBeginEvent(self, e):
         t=self.step_id*self.deltaT # get current time step
         pressures = np.zeros(len(self.pressureConstraints))
@@ -124,7 +125,6 @@ class PressureConstraintController_fullBodyROMPC(Sofa.Core.Controller):
                 # find n_redLocal closest points in full centerline closest to the discretized point in the x direction and the average z value
                 idx = np.argsort((centerline_t[:,0]-xmin-i*dx)**2+np.abs(centerline_t[:,2]-zavg)**2)[0:self.n_redLocal]
     
-
                 # populate averaging matrix
                 self.avgMatrix[i,idx] = 1/self.n_redLocal
 
@@ -156,7 +156,7 @@ class PressureConstraintController_fullBodyROMPC(Sofa.Core.Controller):
         ######### solve ROM MPC optimization for control input #########
         # Get reference trajectory in centered frame - The output is ordered such the tip of the tail is in the first spot and the tip of the head is in the last spot. ordering is [x1,z1,x2,z2,...,xn,zn]
         # y_ref = np.zeros((self.n_redCenterline*2,self.T+1))
-        y_ref = self.generateReferenceTrajectory(time = t,T = self.T+1,a_max=250) 
+        y_ref = self.generateReferenceTrajectory(time = t,T = self.T+1,a_max=30) 
         # y_ref = y_ref - self.redCenterlineOffset.reshape(-1,1)
         pressures = self.rhcPolicy.getAction(x_hat,y_ref,pressures)
 
@@ -165,4 +165,19 @@ class PressureConstraintController_fullBodyROMPC(Sofa.Core.Controller):
         for i in range(len(self.pressureConstraints)): # note! Make sure theyre ordered the right way
             self.pressureConstraints[i].value = [pressures[i]]
 
+
+        # Save verification data for debugging
+        if self.saveVerificationSet:
+            filename = config["currentDirectory"]+"data/verificationData/"+"verification" + "_step_" + self.step_id.__str__() + ".hdf5"
+            with h5py.File(filename, 'w') as f:
+                f.create_dataset('k', data=self.step_id)
+                f.create_dataset('centerline', data=centerlinePts_full)
+                f.create_dataset('y', data=y)
+                f.create_dataset('redCenterline', data=redCenterline)
+                f.create_dataset('x_hat', data=x_hat)
+                f.create_dataset('y_ref', data=y_ref)
+                f.create_dataset('u', data=pressures)
+                f.create_dataset('avgMatrix', data=self.avgMatrix)      
+                f.create_dataset('t', data=t)
+        # Update timestep        
         self.step_id += 1

src/rhcPolicy_ERA.py
@@ -65,7 +65,7 @@ class rhcPolicy_ERA():
         n = 22 # number of states
         m = 6 # number of inputs
         p = 40 # number of outputs
-        T = 100 # prediction horizon
+        # T = 50 # prediction horizon
         Q = np.eye(n) # state cost matrix
         R = np.eye(m) # input cost matrix
         P = np.eye(n) # terminal state cost matrix
@@ -78,6 +78,7 @@ class rhcPolicy_ERA():
         self.du = cp.Variable((m, T))
         self.y = cp.Variable((p, T + 1))
         self.y_ref = cp.Parameter((p, T + 1))
+        self.u_max = 0.15
         # Costs and constraints
         cost = 0
         constr = []
@@ -87,7 +88,7 @@ class rhcPolicy_ERA():
 
         for t in range(T):
             # Apply cost for output trajectory
-            cost += cp.sum_squares(self.y[:,t+1]-self.y_ref[:,t+1])
+            cost += 0.0001*cp.sum_squares(self.y[:,t+1]-self.y_ref[:,t+1])
 
             
             # # Only apply cost for odd output indices to penalize the z trajectory error
@@ -97,10 +98,13 @@ class rhcPolicy_ERA():
 
             # if t % 2 == 1:
             # cost_era += cp.sum_squares(y_era[:, t + 1]-y_ref[:,t+1])#+ cp.sum_squares(u[:, t])
-            cost+= cp.sum_squares(0.1*self.du[:, t])
-            constr += [self.x[:, t + 1] == self.A @ self.x[:, t] + self.B @ self.u[:, t+1], cp.norm(self.u[:, t+1], "inf") <= 1]
+            cost+= cp.sum_squares(self.du[:, t])
+            constr += [self.x[:, t + 1] == self.A @ self.x[:, t] + self.B @ self.u[:, t+1], cp.norm(self.u[:, t+1], "inf") <= self.u_max]
             constr += [self.y[:, t + 1] == self.C @ self.x[:, t + 1] + self.D @ self.u[:, t+1]]
             constr += [self.u[:, t+1] == self.u[:, t] + self.du[:, t]]
+            # Probably need to either add a more significant penalty to du or add a constraint to limit change in input
+
+
             # constraints to limit change in input
             # if t > 0:
             #     constr_era += [cp.norm(u_era[:, t] - u_era[:, t - 1],"inf") <= 0.1]
@@ -117,19 +121,19 @@ class rhcPolicy_ERA():
         # 2) Double check the reference trajectory is being computed and passed in correctly
         
 
-
         # set initial conditions for optimization
         self.x0.value = x0.squeeze()
         self.u0.value = u0.squeeze() # use previous control input as initial condition
         self.y_ref.value = y_ref
         # solve optimization problem
         start = time.time()
-        self.rhcOpt.solve(solver='ECOS', verbose=True)
+        self.rhcOpt.solve(solver='SCS', verbose=True)
         end = time.time()
         # get first control input
-        controlInput = self.u.value[:,0]
+        controlInput = self.u.value[:,1]
         logging.info("ControlOptimization")
         logging.info('controlInput: {}'.format(controlInput.squeeze()))
+        print('controlInput: {}'.format(controlInput.squeeze()))
         logging.info('Optimization time: {}'.format(end-start))
         # update timestep
         self.time += self.dt

src/stateEstimator_ERA.py
@@ -39,8 +39,8 @@ class stateEstimator_ERA():
         y = y.reshape(-1,1)
         # Update state estimate
         self.u = np.reshape(u,(self.m,1))
+        self.x_hat = self.A @ self.x_hat + self.B @ self.u - self.L @ (self.y_hat-y)
         self.y_hat = self.C @ self.x_hat
-        self.x_hat = self.A @ self.x_hat + self.B @ self.u - self.L @ (y-self.y_hat)
         if self.logResults:
             logging.info('u: {}'.format(self.u.squeeze()))
             logging.info('x_hat: {}'.format(self.x_hat.squeeze()))

src/util.py
@@ -59,7 +59,7 @@ def reduceCenterline(n, points, N_local = 20,
 
 
 def reduceCenterlineFullEpisode(n, centerlineData, N_local = 20,
-                                permuteCenterlineReduction = False, # If true, that means centerline should be discretized along the z axis instead of the x axis as usual
+                                permuteCenterlineReduction = False, saveAvgMat = True# If true, that means centerline should be discretized along the z axis instead of the x axis as usual
                                 ):
     '''
     reduces the number of points in the centerline data to n discretized points to form full reduced output matrix for the episode
@@ -89,7 +89,7 @@ def reduceCenterlineFullEpisode(n, centerlineData, N_local = 20,
     xmin = np.min(centerline_t[:,0])
     xmax = np.max(centerline_t[:,0])
     # compute average z value of centerline
-    zavg = -240#np.mean(centerline_t[:,2])
+    zavg = 0#np.mean(centerline_t[:,2])
     # find distance between discretization points
     dx = (xmax-xmin)/(n_redCenterline-1)
     # initialize averaging matrix mapping from full centerline to reduced centerline
@@ -97,9 +97,8 @@ def reduceCenterlineFullEpisode(n, centerlineData, N_local = 20,
     # loop over discretized points to find closest points in full centerline
     for i in range(n_redCenterline):
         # find n_redLocal closest points in full centerline closest to the discretized point in the x direction and the average z value
-        idx = np.argsort(np.abs(centerline_t[:,0]-xmin-i*dx)+2*np.abs(centerline_t[:,2]-zavg))[0:n_redLocal]
+        idx = np.argsort((centerline_t[:,0]-xmin-i*dx)**2+np.abs(centerline_t[:,2]-zavg)**2)[0:n_redLocal]
         # idx = np.argsort(np.abs(centerline_t[:,0]-(xmin+i*dx)))[:n_redLocal]
-
         # populate averaging matrix
         avgMatrix[i,idx] = 1/n_redLocal
 
@@ -134,6 +133,8 @@ def reduceCenterlineFullEpisode(n, centerlineData, N_local = 20,
             redCenterline_t = np.flip(redCenterline_t,1)
         # populate reduced centerline matrix
         reducedPoints[i,:] = redCenterline_t.flatten()
+    if saveAvgMat:
+        np.save(config["currentDirectory"] +"avgMatrix.npy",avgMatrix)
 
     return reducedPoints
 
@@ -146,7 +147,7 @@ def cleanData(stateDataFilePathPrefix = "stateExporter_policySeed_0_step_",
               outFilename = "processedData_policySeed_0.npz",
               permuteCenterlineReduction = False, # If true, that means centerline should be discretized along the z axis instead of the x axis as usual
               n_redCenterline = 20, # number of discretized points in reduced centerline
-              n_redLocal = 3, # number of points to average over closest to the discretized points
+              n_redLocal = 20, # number of points to average over closest to the discretized points
               ):
     
     # Parameters for reduced centerline
@@ -431,11 +432,11 @@ if __name__ == '__main__':
     # cleanDataMultiEpisodes(numEpisodes=50)
     # cleanData(numTimeSteps=3000, outFilename="processedData_policySeed_0.npz",permuteCenterlineReduction=False)
     
-    # cleanDataMultiEpisodes(numEpisodes=10,numTimeSteps=2000,permuteCenterlineReduction=False)
+    cleanDataMultiEpisodes(numEpisodes=1,numTimeSteps=500,permuteCenterlineReduction=False, )
     # generateDataSetFromProcessedNPZs(saveMatlab=False, savehdf5 = True)
     # animateReducedCenterline(numTimeSteps=1999,saveName="reducedCenterline_fullAssembly_constrained.mp4")
 
 
     # mat2hdf5(filepath=config["currentDirectory"] +"data/archivedDataSets/FullAssembly_Constrained_FullSetForICRA/trainingSet.mat", outfilePath=config["currentDirectory"] +"data/archivedDataSets/FullAssembly_Constrained_FullSetForICRA/trainingSet_test.hdf5")
 
-    combineDataSets()
+    # combineDataSets()